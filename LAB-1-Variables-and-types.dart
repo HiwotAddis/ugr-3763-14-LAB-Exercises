70b01f3f252d7234dc6cd564ccd4ac0102c12e7e
HiwotAddis <133164244+HiwotAddis@users.noreply.github.com>
2024-03-08 14:48:54 +0300

    Create LAB-1-Variables-and-types.dart

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name= 'Hiwot Addis.';
+  int Age= 21;
+  String favoriteColor = "Brown";
+  
+  print('My name is $name');
+  print('I am $Age years old.');
+  print('My favorite color is $favoriteColor.');
+}
